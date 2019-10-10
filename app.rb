8f1ac99bd542af9e81e7586b88c562c0a9fd6070
Chandon Porter <cneilporter94@gmail.com>
2019-10-10 14:09:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Chandon"
+end
+
+  get '/hometown' do 
+  "My hometown is La Marque"
+end
+
+  get '/favorite-song' do 
+  "My favorite song is Studio"
+  end
 end
